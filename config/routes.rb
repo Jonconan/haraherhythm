b825f38ba2b2a95e8fa4d981a7f4e91c60e5947d
Jonconan <jun.miyahara.1020@gmail.com>
2021-09-06 18:53:24 +0900

    routes登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :artists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
-  # devise_for :artists, :controllers => {
-  #   :registrations => 'artists/registrations',
-  #   :sessions => 'artists/sessions'
-  # }
+  devise_for :artists, :controllers => {
+    :registrations => 'artists/registrations',
+    :sessions => 'artists/sessions'
+  }
 
   resources :lives, only: [] do
     get '/', action: :index
