6766f2850c1588979912af3f89356dc31a01664d
Jonconan <jun.miyahara.1020@gmail.com>
2021-09-09 21:23:05 +0900

    プロフィール編集画面

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,4 +1,22 @@
 class MypageController < ApplicationController
   def index
   end
+
+  def edit
+    @artist = current_artist
+  end
+
+  def update
+    render json: artist_params and return
+  end
+
+  protected
+
+  def artist_params
+    params.require(:artist).permit(
+      :email, :name, :nickname,
+      :birthday, :postal_code, :address_1, :address_2, :address_3,
+      :website_url, :description
+    )
+  end
 end
