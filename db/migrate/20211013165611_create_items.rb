e44233731300fe1dfbfd9bc94aeee167c68412a5
Jonconan <jun.miyahara.1020@gmail.com>
2021-10-14 02:07:47 +0900

    諸々migrate

---
+++ db/migrate/20211013165611_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.references :artist, foreign_key: true
+
+      t.string :name, null: false, comment: "商品名"
+      t.integer :price, null: false, comment: "価格"
+      t.integer :sales_format_id, null: false, comment: "販売形式"
+      t.integer :delivery_days, comment: "発送までの日数"
+      t.text   :description, comment: "商品紹介"
+
+      t.timestamps
+    end
+  end
+end
