bf6e946467e9c50b9b9f5e30c12bc45effbfce12
Jonconan <jun.miyahara.1020@gmail.com>
2021-10-13 23:44:38 +0900

    画像が表示されない問題

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'haraherhythm'
-    config.asset_host = 'https://s3.amazonaws.com/haraherhythm'
+    config.asset_host = 'https://haraherhythm.s3.amazonaws.com'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
