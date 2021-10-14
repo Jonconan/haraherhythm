136b42c4a34eeae06d94746e3b58b627264b0e9e
Jonconan <jun.miyahara.1020@gmail.com>
2021-10-14 15:37:34 +0900

    releation設定修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,5 @@ class Item < ApplicationRecord
   belongs_to :artist
   mount_uploader :thumbnail, ImageUploader
 
-  has_one :master_sales_format
-
-  def sales_format
-    return nil unless sales_format_id.present?
-    MasterSalesFormat.find_by(id: sales_format_id).name
-  end
+  belongs_to :sales_format, class_name: 'MasterSalesFormat', foreign_key: :sales_format_id
 end
