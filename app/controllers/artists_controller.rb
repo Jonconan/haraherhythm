b24db299973f2cb33056b73f7ea4fe3883d7a02f
Jonconan <jun.miyahara.1020@gmail.com>
2021-10-16 23:32:51 +0900

    ログインしてない状態でフォローを押したときのバグ修正

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,9 +9,9 @@ class ArtistsController < ApplicationController
   def follow
     artist = Artist.find_by(id: params[:artist_id])
     # もしアーティストがいなかったらトップへ飛ばす（不正リクエスト）
-    redirect_to root_path unless artist.present?
+    redirect_to root_path and return unless artist.present?
     # ユーザーでログインしていない状態だったら何もせず戻す
-    redirect_to artist_path(artist_id: artist.id) unless current_user.present?
+    redirect_to artist_path(artist_id: artist.id) and return unless current_user.present?
     # このアーティストをフォローする
     current_user.user_artists.create(artist_id: artist.id)
     redirect_to artist_path(artist_id: artist.id)
@@ -21,9 +21,9 @@ class ArtistsController < ApplicationController
   def unfollow
     artist = Artist.find_by(id: params[:artist_id])
     # もしアーティストがいなかったらトップへ飛ばす（不正リクエスト）
-    redirect_to root_path unless artist.present?
+    redirect_to root_path and return unless artist.present?
     # ユーザーでログインしていない状態だったら何もせず戻す
-    redirect_to artist_path(artist_id: artist.id) unless current_user.present?
+    redirect_to artist_path(artist_id: artist.id) and return unless current_user.present?
     # このアーティストのフォローを解除する
     current_user.user_artists.where(artist_id: artist.id).delete_all
     redirect_to artist_path(artist_id: artist.id)
