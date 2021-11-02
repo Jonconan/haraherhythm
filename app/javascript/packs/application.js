b5a0aa314ca94ce527cbe029995c410a92d6c1cd
Jonconan <jun.miyahara.1020@gmail.com>
2021-11-02 20:29:23 +0900

    wip cropper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,16 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require cropper.min.js
+//= require jquery-cropper.min.js
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require popper
+//= require bootstrap-sprockets
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
